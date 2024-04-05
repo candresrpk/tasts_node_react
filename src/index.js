94ac7c8baa26134804ee4eb18113f9b1793f9cfd
carlos reyes <andrespk3000@gmail.com>
2024-04-05 01:16:35 -0500

    express configuration basics

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import app from "./app.js";
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`listening on port : ${port}`);
+}).on('error', (e) => {
+    console.log('Error happened:', e.message)
+})
